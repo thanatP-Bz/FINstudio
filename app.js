d342cad146abfc5c8525c2a76af0411c75e2d3f6
Thanat <bztzn512@gmail.com>
2022-01-20 20:55:20 -0500

    addMoreFucntion

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const toggleButton = document.querySelector(".nav-toggle");
 const sidebar = document.querySelector(".side-bar");
 const showcase = document.querySelector(".showcase");
 
+const logo = document.querySelector(".logo");
+const navlinks = document.querySelector(".nav-links");
+
 const icon1 = document.querySelector(".icon-1");
 const icon2 = document.querySelector(".icon-2");
+
+window.addEventListener("DOMContentLoaded", () => {
+  showcase.classList.add("reveal-showcase");
+  logo.classList.add("reveal-logo");
+  navlinks.classList.add("reveal-nav");
+});
 
 toggleButton.addEventListener("click", () => {
   sidebar.classList.toggle("show-side_bar");
   showcase.classList.toggle("showcase-hidden");
-  /*   icon1.classList.add("hidden"); */
+
   if (!icon1.classList.contains("hidden")) {
     icon1.classList.add("hidden");
     icon2.classList.remove("hidden");
@@ -17,3 +26,15 @@ toggleButton.addEventListener("click", () => {
     icon2.classList.add("hidden");
   }
 });
+
+const reset = () => {
+  const viewUpdate = window.innerWidth;
+
+  if (viewUpdate > 800) {
+    showcase.classList.remove("showcase-hidden");
+    sidebar.classList.remove("show-side_bar");
+    icon1.classList.remove("hidden");
+    icon2.classList.add("hidden");
+  }
+};
+window.onresize = reset;
---
+++ scroll.js
@@ -0,0 +1,4 @@
+const header = document.getElementById("#header");
+const content = document.querySelector(".content");
+const intial = document.body.clientWidth;
+console.log(intial);
