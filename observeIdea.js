63af059ced0ec343b0f6615f710111c311e3b579
Thanat <bztzn512@gmail.com>
2022-01-22 21:49:31 -0500

    animattion

--- observeIdea.js
+++ observeIdea.js
@@ -1,14 +1,25 @@
 const idea = document.querySelector(".idea");
 const gridBox = document.querySelectorAll(".grid-box");
+const rect = idea.getBoundingClientRect();
+console.log(rect);
 
-const revealBox = (entries) => {
-  entries.forEach((item) => {
-    console.log(item);
+const revealBox = (entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.remove("reveal-hidden");
+    }
   });
 };
 
 const ideaObserver = new IntersectionObserver(revealBox, {
-  root: null,
-  threshold: 0,
+  rootMargin: `0px 0px -150px 0px`,
+  threshold: 1,
 });
-ideaObserver.observe(idea);
+
+gridBox.forEach((item) => {
+  ideaObserver.observe(item);
+  item.classList.add("reveal-hidden");
+});
